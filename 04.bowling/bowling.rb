56f6bd4bc3f3e0aaecb25375ff2bdc9e448bbbcd
Yuki Fushihara <y.f0902.1120@gmail.com>
2021-09-20 10:03:47 +0900

    ボウリング課題完了

---
+++ 04.bowling/bowling.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+# 倒れたピンの本数をカウント
+score = ARGV[0] # コマンドライン引数の取得
+scores = score.split(',') # 取得した値をカンマ(,)で文字列を分割して結果を配列に格納
+shots = [] # 空の変数shotsを作成、取得された値が代入される
+scores.each do |_s| # 変数sccoreの値がXの場合はshotsに［10，0］が代入され、それ以外は数値へ変換され、代入される
+  if score == 'X'
+    shots << 10
+    shots << 0
+  else
+    shots << score.to_i
+  end
+end
+
+# フレーム毎に分割
+# 空の変数framesを作成、取得された値が代入される
+frames = []
+frame = []
+# conunt = 0
+shots.each do |shot|
+  if frames.length > 9 # 10フレーム用の処理。10フレームを迎えていたら最後のframeにshotを追加する。
+    frames.last << shot
+  elsif shot == 'X' # ストライク
+    frame << 10
+    frames << frame # framesにframeを追加する
+    frame = [] # 次のフレームのために配列を空にする
+  elsif frame.count == 1 # 2投目用の処理。frameの要素が1つだったら2投目を追加。
+    frame << shot
+    frames << frame
+    frame = []
+  else
+    frame << shot
+  end
+end
+
+# スコア計算
+point = 0
+frames.each_with_index do |_f, i| # 変数frameは配列の要素、変数iは配列の要素の順序（インデックス）
+  point += if i < 8 && frames[i][0] == 10 && frames[i + 1][0] == 10 # 連続ストライクの場合
+             20 + frames[i + 2][0]
+
+           elsif i == 8 && frames[i][0] == 10 && frames[i + 1][0] == 10 # 9レーン目の次もストライク
+             10 + frames[i + 1][0] + frames[i + 1][2]
+
+           elsif i == 8 && frames[i][0] == 10 # 9レーン目の処理
+             10 + frames[i + 1][0] + frames[i + 1][1]
+
+           elsif i < 9 && frames[i][0] == 10 # ストライクの場合
+             10 + frames[i + 1][0] + frames[i + 1][1] # 10 + 次のフレームの1投目 + 次のフレームの2投目
+
+           elsif i < 9 && frame.sum == 10 && frames[0] != 10 # スペアの場合
+             10 + frames[i + 1][0] # 次のフレームの1投目
+
+           else
+             frame.sum # その他の場合はフレーム内を足す
+           end
+end
+
+p frames
+puts point
