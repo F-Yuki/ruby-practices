501bca88c7858b580da9aa1e4eb12fd4d2137c80
Yuki Fushihara <y.f0902.1120@gmail.com>
2022-01-02 08:27:27 +0900

    lsコマンドを作る1課題修正完了１

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -2,18 +2,20 @@
 
 dir = Dir.glob('*')
 
-max_column = 3.0
-total_files = dir.size
-number_of_lines = (total_files.to_f / max_column).ceil(0)
+max_column_count = 3.0
+total_folders_count = dir.size
+number_of_lines = (total_folders_count.to_f / max_column_count).ceil(0)
 
 files = []
 dir.each_slice(number_of_lines) do |n|
   files << n
 end
 
-if files.size >= max_column && total_files % max_column != 0
-  (max_column - total_files % max_column).to_i.times do
-    files[-1] << nil
+last_line_lack_element_count = (max_column_count - total_folders_count % max_column_count).to_i
+
+if last_line_lack_element_count != 0
+  last_line_lack_element_count.times do
+    files.last << nil
   end
 end
 
