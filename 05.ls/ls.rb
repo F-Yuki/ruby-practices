0ecc05d77c341f7c80d24ddc8a3b5f0425352138
Yuki Fushihara <y.f0902.1120@gmail.com>
2022-02-06 08:50:21 +0900

    lsコマンドを作る1課題修正完了5

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -7,13 +7,17 @@ total_elements_count = dir.size
 number_of_lines = (total_elements_count.to_f / max_column_count).ceil(0)
 
 files = []
-dir.each_slice(number_of_lines) do |n|
-  files << n
+if number_of_lines == 0
+    return
+else
+  dir.each_slice(number_of_lines) do |n|
+    files << n
+  end
 end
 
 last_line_lack_element_count = (max_column_count - total_elements_count % max_column_count).to_i
 
-if last_line_lack_element_count != 3
+if files.size >= max_column_count && total_elements_count % max_column_count != 0
   last_line_lack_element_count.times do
     files.last << nil
   end
