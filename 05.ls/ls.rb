0736e625f95c5db5986af12cf1912cfa3f7ca9e9
Yuki Fushihara <y.f0902.1120@gmail.com>
2022-04-03 14:07:23 +0900

    lsコマンドを作る1課題修正完了6

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -2,37 +2,34 @@
 
 dir = Dir.glob('*')
 
-max_column_count = 3.0
+MAX_COLUMN_COUNT = 3
 total_elements_count = dir.size
-number_of_lines = (total_elements_count.to_f / max_column_count).ceil(0)
+number_of_lines = (total_elements_count.to_f / MAX_COLUMN_COUNT).ceil(0)
+last_line_lack_element_count = (MAX_COLUMN_COUNT - total_elements_count % MAX_COLUMN_COUNT).to_i
 
-files = []
-if number_of_lines == 0
-    return
-else
+columns = []
+if total_elements_count != 0
   dir.each_slice(number_of_lines) do |n|
-    files << n
+    columns << n
   end
 end
 
-last_line_lack_element_count = (max_column_count - total_elements_count % max_column_count).to_i
-
-if files.size >= max_column_count && total_elements_count % max_column_count != 0
+if !columns.empty? && columns.last.size != columns[0].size
   last_line_lack_element_count.times do
-    files.last << nil
+    columns.last << nil
   end
 end
 
-def show_files(dir, files)
-  sort_files = files.transpose
+def show_files(dir, columns)
+  transposed_columns = columns.transpose
   long_name = dir.max_by(&:size)
   margin = 3
-  sort_files.each do |sort_file|
-    sort_file.each do |f|
+  transposed_columns.each do |transposed_column|
+    transposed_column.each do |f|
       print f.to_s.ljust(long_name.size + margin)
     end
     print "\n"
   end
 end
 
-show_files(dir, files)
+show_files(dir, columns)
